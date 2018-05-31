280aff0e2b2c9ed1bbb84a7843030ef997941eca
Gaganbasra55 <38173348+Gaganbasra55@users.noreply.github.com>
2018-05-31 18:32:03 -0400

    questions created

--- src/exercise1/Test.java
+++ src/exercise1/Test.java
@@ -1,5 +1,17 @@
 package exercise1;
 
+import java.security.SecureRandom;
+
 public class Test {
+    // random-number generation
+    private final SecureRandom randomNumbers = new SecureRandom();
+    private int rightAnswers, wrongAnswers = 0;
+    //set of questions
+    private final String[] questions ={"The object is created with new keyword:",
+                                        "These are rules that must be followed when writing a program:",
+                                        "The java compiler generates:",
+                                        "Which keyword is used to declare a named constant?"
+                                        "Every complete statement ends with:"};
+
     
 }
