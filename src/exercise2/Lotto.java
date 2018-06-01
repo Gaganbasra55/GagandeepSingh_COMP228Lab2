fce675aa6e86bb6903629c1daa8a2da3be2e3d5e
Gaganbasra55 <38173348+Gaganbasra55@users.noreply.github.com>
2018-05-31 20:21:44 -0400

    lotto class created with 3 numbers and getArray()

---
+++ src/exercise2/Lotto.java
@@ -0,0 +1,29 @@
+package exercise2;
+
+public class Lotto {
+    //Array
+    private int[] lottoNumbers = new int[3];
+
+    //Constructor receiving random numbers
+    public Lotto(int num1, int num2, int num3){
+
+        this.lottoNumbers[0] = num1;
+        this.lottoNumbers[1] = num2;
+        this.lottoNumbers[2] = num3;
+    }
+    //Method that returns the array
+    public int[] getArray(){
+
+
+        return this.lottoNumbers;
+    }
+
+    public int sum(){
+
+        return (this.lottoNumbers[0]+this.lottoNumbers[1]+this.lottoNumbers[2]);
+
+    }
+
+}
+
+}
