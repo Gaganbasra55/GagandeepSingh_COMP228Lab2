5de499fcb4e6f41cc75e0c879df078763e2090db
Gaganbasra55 <38173348+Gaganbasra55@users.noreply.github.com>
2018-05-31 21:48:51 -0400

    ex3 started and completed ex2

--- src/exercise2/LottoTest.java
+++ src/exercise2/LottoTest.java
@@ -18,4 +18,45 @@ public class LottoTest {
 
         return 1 + randomNumbers.nextInt(9);
     }
-    
+    private static void lottoGame() {
+        //Boolean value to control a loop
+        boolean value = false;
+        int selectedNumber = 0;
+        //String that will have a format to output information
+        String runReport = "";
+        int times = 0;
+        JOptionPane.showMessageDialog(null, "Time to play Lotto", "Lotto Game", 1);
+
+        selectedNumber = Integer.parseInt(JOptionPane.showInputDialog(null, "enter a number between 3 to 27"));
+
+        while (times < 5) {
+
+
+            //Creating Lotto Object with random numbers
+            Lotto l1 = new Lotto(generateNumber(), generateNumber(), generateNumber());
+            //auxiliary array to display information
+            int[] aux = new int[3];
+            aux = l1.getArray();
+            //Formatting String
+            runReport = String.format("\n1st number = %d%n2nd number = %d%n3rd number = %d"
+                                    + "\n\nThe number you entered is: %d%n"
+                                    + "The sum of the numbers is: %d", aux[0], aux[1], aux[2], selectedNumber, l1.sum());
+
+            //Showing the generated numbers
+            JOptionPane.showMessageDialog(null, runReport, "Numbers generated", 1);
+
+            //Win condition
+            if (l1.sum() == selectedNumber) {
+                JOptionPane.showMessageDialog(null, "You Won", "win", 1);
+                break;
+            } else
+                times++;
+        }
+        //Lose condition
+        if (times == 5)
+            JOptionPane.showMessageDialog(null, "Computer Wins", "lose", 0);
+
+    }
+
+}
+
