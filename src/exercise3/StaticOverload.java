e79ed91327a1e3a967bbb0339b95d87a53e31950
Gaganbasra55 <38173348+Gaganbasra55@users.noreply.github.com>
2018-06-01 21:09:27 -0400

    StaticOverload verified

--- src/exercise3/StaticOverload.java
+++ src/exercise3/StaticOverload.java
@@ -2,25 +2,25 @@ package exercise3;
 
 public class StaticOverload {
 
-    //method to calculate area of square
+    //method to calculate perimeter of square
     public static int perimeter(int a){
         return (4*a);
     }
 
-    //method to calculate area of Rectangle
+    //method to calculate perimeter of Rectangle
     public static int perimeter(int a,int b){
         return (2*(a+b));
     }
 
-    //method to calculate area of Triangle
+    //method to calculate perimeter of Triangle
     public static int perimeter(int a,int b,int c){
         return ((a+b+c));
     }
 
     public static void main(String[] args) {
      StaticOverload object=new StaticOverload();
-        System.out.println("Perimeter of Square = "+perimeter(5));
-        System.out.println("Perimeter of Rectangle = "+perimeter(8,12));
-        System.out.println("Perimeter of Triangle = "+perimeter(9,17,11));
+        System.out.println("Perimeter of Square = "+object.perimeter(5));
+        System.out.println("Perimeter of Rectangle = "+object.perimeter(8,12));
+        System.out.println("Perimeter of Triangle = "+object.perimeter(9,17,11));
     }
 }
